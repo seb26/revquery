cc4f7432734262608c0f85388b761e2774d618e4
seb26 <unleashed26@gmail.com>
2012-01-31 12:54:35 +1100

    add new parser hooks {{#file-ts:}} and {{#filepath-ts:}}, for returning the timestamp and the path to the file URL with appended timestamp.

--- RevQuery.php
+++ RevQuery.php
@@ -22,19 +22,66 @@ $wgExtensionCredits['parserhook'][] = array(
 /*
  *
  * NOTE:
-
- * This extension does not use any hooks, and has no functionality introduced
- * with this particular file. URL timestamps are implemented by patching files
- * in MediaWiki's standard include files.
  *
- * $wgRevQueryTimestamps must be set to true to use this feature.
+ * This file does not append timestamps to any URLs; this is done by patching
+ * files in MediaWiki's standard include files. To disable this feature,
+ * $wgRevQueryTimestamps can be set to false in LocalSettings.php. To remove
+ * the RevQuery changes, you can undo the patch using the -R flag with GNU patch.
+ * Alternatively, you can redownload the MediaWiki archive and extract the
+ * affected files to your install directory.
  *
- * This file exists to include a credit in [[Special:Version]]. This extension
- * cannot be properly installed by just including this file; your MediaWiki
- * install must first be patched. You can find the appropriate patch file
+ * Timestamps cannot be appended to URLs by simply including this file; your
+ * MediaWiki install must first be patched. You can find the appropriate patch
  * corresponding to your wiki's version at <https://github.com/seb26/revquery>.
  *
  */
 
-/* This is the default setting. Please change this value in LocalSettings.php instead. */
-$wgRevQueryTimestamps = false;
+# This is the default setting.
+# To disable timestamps, set this to false in LocalSettings.php instead.
+$wgRevQueryTimestamps = true;
+
+# {{filetimestamp:}} and {{filepathtimestamp:}} functions
+
+$wgHooks['LanguageGetMagic'][] = 'wfRevQuery_LanguageGetMagic';
+$wgHooks['ParserFirstCallInit'][] = 'wfRevQuery_ParserFirstCallInit';
+
+function wfRevQuery_ParserFirstCallInit( $parser ) {
+	$parser->setFunctionHook( 'file-ts', 'wfRevQuery_file_ts', SFH_OBJECT_ARGS );
+	$parser->setFunctionHook( 'filepath-ts', 'wfRevQuery_filepath_ts', SFH_OBJECT_ARGS );
+	return true;
+}
+
+function wfRevQuery_LanguageGetMagic( &$magicWords, $langCode ) {
+	$magicWords['file-ts'] = array( 0, 'file-ts' );
+	$magicWords['filepath-ts'] = array( 0, 'filepath-ts' );
+	return true;
+}
+
+function wfRevQuery_file_ts( $parser, $frame, $args ) {
+	$file = wfRevQuery_getFile( $parser, trim( $frame->expand( $args[0] ) ) );
+	return $file ? $file->getTimestamp() : '';
+}
+
+function wfRevQuery_filepath_ts( $parser, $frame, $args ) {
+	$file = wfRevQuery_getFile( $parser, trim( $frame->expand( $args[0] ) ) );
+	if ( $file ) {
+		global $wgServer;
+		return $wgServer . $file->getURL() . '?t=' . $file->getTimestamp();
+	}
+	else {
+		return '';
+	}
+}
+
+function wfRevQuery_getFile( &$parser, $text ) {
+	$title = Title::newFromText( $text, NS_FILE );
+	$file = $parser->fetchFile( $title );
+
+	if ( $file && $file->exists() ) {
+		return $file;
+	}
+	else {
+		return false;
+	}
+
+}
